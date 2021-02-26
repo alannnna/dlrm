cd9168094c770f057f6afcce803b37ed4078ec9c
Alanna Tempest <atem@fb.com>
2021-02-26 15:14:36 -0500

    remove lr scheduler logic

--- dlrm_small.py
+++ dlrm_small.py
@@ -76,7 +76,6 @@ from torch._ops import ops
 from torch.autograd.profiler import record_function
 from torch.nn.parallel.scatter_gather import gather, scatter
 from torch.nn.parameter import Parameter
-from torch.optim.lr_scheduler import _LRScheduler
 
 # mixed-dimension trick
 from tricks.md_embedding_bag import PrEmbeddingBag, md_solver
@@ -96,43 +95,6 @@ exc = getattr(builtins, "IOError", "FileNotFoundError")
 def unpack_batch(b):
     # Experiment with unweighted samples
     return b[0], b[1], b[2], b[3], torch.ones(b[3].size()), None
-
-
-class LRPolicyScheduler(_LRScheduler):
-    def __init__(self, optimizer, num_warmup_steps, decay_start_step, num_decay_steps):
-        self.num_warmup_steps = num_warmup_steps
-        self.decay_start_step = decay_start_step
-        self.decay_end_step = decay_start_step + num_decay_steps
-        self.num_decay_steps = num_decay_steps
-
-        if self.decay_start_step < self.num_warmup_steps:
-            sys.exit("Learning rate warmup must finish before the decay starts")
-
-        super(LRPolicyScheduler, self).__init__(optimizer)
-
-    def get_lr(self):
-        step_count = self._step_count
-        if step_count < self.num_warmup_steps:
-            # warmup
-            scale = 1.0 - (self.num_warmup_steps - step_count) / self.num_warmup_steps
-            lr = [base_lr * scale for base_lr in self.base_lrs]
-            self.last_lr = lr
-        elif self.decay_start_step <= step_count and step_count < self.decay_end_step:
-            # decay
-            decayed_steps = step_count - self.decay_start_step
-            scale = ((self.num_decay_steps - decayed_steps) / self.num_decay_steps) ** 2
-            min_lr = 0.0000001
-            lr = [max(min_lr, base_lr * scale) for base_lr in self.base_lrs]
-            self.last_lr = lr
-        else:
-            if self.num_decay_steps > 0:
-                # freeze at last, either because we're after decay
-                # or because we're between warmup and decay
-                lr = self.last_lr
-            else:
-                # do not adjust
-                lr = self.base_lrs
-        return lr
 
 
 ### define dlrm in PyTorch ###
@@ -452,10 +414,6 @@ def run():
     parser.add_argument("--print-wall-time", action="store_true", default=False)
     parser.add_argument("--debug-mode", action="store_true", default=False)
     parser.add_argument("--enable-profiling", action="store_true", default=False)
-    # LR policy
-    parser.add_argument("--lr-num-warmup-steps", type=int, default=0)
-    parser.add_argument("--lr-decay-start-step", type=int, default=0)
-    parser.add_argument("--lr-num-decay-steps", type=int, default=0)
 
     global args
     global nbatches
@@ -659,12 +617,6 @@ def run():
         # print(dlrm)
 
     optimizer = torch.optim.SGD(dlrm.parameters(), lr=args.learning_rate)
-    lr_scheduler = LRPolicyScheduler(
-        optimizer,
-        args.lr_num_warmup_steps,
-        args.lr_decay_start_step,
-        args.lr_num_decay_steps,
-    )
 
     ### main loop ###
 
@@ -721,7 +673,6 @@ def run():
 
                     # optimizer
                     optimizer.step()
-                    lr_scheduler.step()
 
                 t2 = time.time()
                 total_time += t2 - t1
